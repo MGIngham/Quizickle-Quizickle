eb9fbc4ee04560cbdef467db73b9a8a963397aa5
Matt Ingham <matt.g.ingham@gmail.com>
2020-10-25 10:24:20 +0000

    Button directive added

--- src/app/app-button.directive.ts
+++ src/app/app-button.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appAppButton]'
 })
 export class AppButtonDirective {
 
-  constructor() { }
+  constructor(el: ElementRef) { 
+    el.nativeElement.style.backgroundColor = "rgb(40, 156, 233)";
+    el.nativeElement.style.color = "#fff";
+    el.nativeElement.style.margin = "30px";
+    el.nativeElement.style.fontWeight = "900";
+    el.nativeElement.style.fontFamily = "Noto Sans, sans-serif";
+  };
 
 }
